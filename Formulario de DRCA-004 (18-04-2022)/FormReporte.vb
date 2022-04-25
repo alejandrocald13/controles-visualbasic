039e0e8751c560d2a98cbe4bff0b2814f1d67ee1
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2022-04-25 11:16:55 -0600

    Revert "Formulario"
    This reverts commit df7d77ab458ffcd696fc1fc79bebf6ee3f925bad.

--- Formulario de DRCA-004 (18-04-2022)/FormReporte.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class FormReporte
-    Private Sub FormReporte_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label1.Text = Form1.tbNameCom.Text
-        Label2.Text = Form1.tbNIT.Text
-        Label3.Text = Form1.tbFabDireccionExacta.Text
-    End Sub
-End Class
